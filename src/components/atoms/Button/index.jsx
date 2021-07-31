d89705d64490957e2f25395c86098703d1db2c61
RYOTAN9688 <ryota8813@gmail.com>
2021-07-31 14:16:21 +0900

    ボタンコンポーネント完成

--- src/components/atoms/Button/index.jsx
+++ src/components/atoms/Button/index.jsx
@@ -3,41 +3,41 @@ import PropTypes from "prop-types";
 import styled, { css } from "styled-components";
 
 export const sizes = ["s", "m", "l"];
-//fullWidth=trueで幅100％
+//fullWidth=trueで幅100% `
 const Root = styled.button`
-${({ fullWidth }) => fullWidth && css`width:100%`};
-appearance:none; 
-border : 0;
+${({ fullWidth }) => fullWidth && css`width: 100%`};
+appearance: none; 
+border: 0;
 border-radius: 4px;
 background: #ddd;
-color : #000;
-font-weight : bold ;
-cursor : pointer;
-:hover{
-    background:#eee;
+color: #000;
+font-weight: bold;
+cursor: pointer;
+:hover {
+    background: #eee;
 }
 :focus {
     outline: none;
-    box-shadow: 0 0 0 4px "cbd6ee;
+    box-shadow: 0 0 0 4px #cbd6ee;
 }
-transition : baclground-color .ls linear;
-${({ size }) => {
+transition: background-color .ls linear;
+ ${({ size }) => {
         switch (size) {
-            case "s":
+            case 's':
                 return css`
-                font-size : 12px;
-                padding: 6px 12px;
-                `;
-            case "l":
+           font-size: 12px;
+           padding: 6px 12px;
+        `;
+            case 'l':
                 return css`
-                font-size : 18px;
-                padding : 10px 18px;
-                `;
+           font-size: 18px;
+           padding: 10px 18px;
+        `;
             default:
                 return css`
-                font-size : 16px;
-                padding : 6px 16px;
-                `;
+           font-size: 16px;
+           padding: 6px 16px;
+        `;
         }
     }};
 `;
@@ -64,6 +64,7 @@ const Button = ({
 
 Button.propTypes = {
     className: PropTypes.string,
+    children: PropTypes.node.isRequired,
     type: PropTypes.string,
     size: PropTypes.oneOf(sizes),
     fullWidth: PropTypes.bool,
@@ -78,4 +79,4 @@ Button.defaultProps = {
     onclick: null,
 }
 
-export const Button;
+export default Button;
