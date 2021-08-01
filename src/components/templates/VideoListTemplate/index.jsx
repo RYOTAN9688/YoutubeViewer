6d6e3eb36e9d3f5888283facc7cca742d43b1bc3
RYOTAN9688 <ryota8813@gmail.com>
2021-08-01 09:04:51 +0900

    Template作成

--- src/components/templates/VideoListTemplate/index.jsx
+++ src/components/templates/VideoListTemplate/index.jsx
@@ -9,9 +9,9 @@ const Root = styled.div`
 `;
 
 const HeaderWrapper = styled.div`
-    max-width: 720px;
+    max-width: 100%;
     margin: auto;
-    border-bottom: ipx solid #ccc;
+    border-bottom: 1px solid #ccc;
 `;
 
 const SearchFormWrapper = styled.div`
@@ -19,15 +19,15 @@ const SearchFormWrapper = styled.div`
     margin: auto;
 `;
 
-const VideoListWrapper = styled.div`
+const VideosListWrapper = styled.div`
     max-width: 720px;
     margin: auto;
 `;
 
-const VideoListTemplate = ({
+const VideosListTemplate = ({
     headerContents,
-    SearchFormContents,
-    videoListContents,
+    searchFormContents,
+    videosListContents,
     onScrollEnd,
 }) => {
     //scrollハンドラをDOMに設定したので副作用ある処理が走る。
@@ -42,8 +42,8 @@ const VideoListTemplate = ({
         //スクロール時のイベントハンドラ
         const scrollHandler = ({ target: { scrollingElement } }) => {
             //一番下までスクロールされたかどうか判断し、一番下までスクロールされたらonScrollEndを呼び出す
-            const { scrollTop, scrollHeight, clientheight } = scrollingElement;
-            if (scrollTop < scrollHeight - clientheight) {
+            const { scrollTop, scrollHeight, clientHeight } = scrollingElement;
+            if (scrollTop < scrollHeight - clientHeight) {
                 //スクロールした位置が一番下でない場合は何もしない
                 return;
             }
@@ -51,13 +51,46 @@ const VideoListTemplate = ({
             onScrollEnd();
         };
         //イベントハンドラの設定
-        window.document.addEventListener("scroll", sclollHandler);
+        window.document.addEventListener("scroll", scrollHandler);
         //useEffectの第一引数の関数では、関数を返している
         return () => {
             //コンポーネントのアンマウント時に設定したイベントハンドラを削除する
+            //useEffctのクリーンアップのためのオプションの仕組み。
+            //返した関数は、コンポーネントをアマウントしたとき、また第二引数が変更されたときに実行
+            //この仕組みによりuseEffectの副作用（DOMにハンドラを追加した処理）に対して、
+            //不要になったときに元に戻すことができる
             window.document.removeEventListener("scroll", scrollHandler);
         };
         //第二引数にonScrollEndを指定。onScrollEndの値が変わるとuseEffectの第一引数の処理が実行される
         //onScrollEndが変更されると、変更後のonScrollEndを使って再度イベントハンドラの設定処理が実行
     }, [onScrollEnd]);
+
+    return (
+        <Root>
+            <HeaderWrapper>
+                {headerContents}
+            </HeaderWrapper>
+            <SearchFormWrapper>
+                {searchFormContents}
+            </SearchFormWrapper>
+            <VideosListWrapper>
+                {videosListContents}
+            </VideosListWrapper>
+        </Root>
+    );
 };
+
+VideosListTemplate.propTypes = {
+    headerContents: PropTypes.node,//renderできるものかチェック
+    SearchFormContents: PropTypes.node,
+    videosListContents: PropTypes.node.isRequired,//renderできるものかをチェック（必須）
+    onScrollEnd: PropTypes.func,//関数であるかチェック
+};
+
+VideosListTemplate.defaultProps = {
+    headerContents: null,
+    SearchFormContents: null,
+    onScrollEnd: null,
+}
+
+export default VideosListTemplate;
