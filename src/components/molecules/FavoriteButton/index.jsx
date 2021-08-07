13ff7d8061c0ca72752474cf1d620c50568a353a
RYOTAN9688 <ryota8813@gmail.com>
2021-08-07 18:03:04 +0900

    お気に入りボタン作成中

--- src/components/molecules/FavoriteButton/index.jsx
+++ src/components/molecules/FavoriteButton/index.jsx
@@ -32,3 +32,58 @@ FavoriteButtonPresenter.defaultProps = {
     isFavorite: false,
     onClick: null,
 };
+
+
+const FavoriteButtonContainer = ({
+    className,
+    videoId,
+    api,
+    presenter,
+}) => {
+    const { state: { ids: favoriteIds }, dispatch } = useContext(FavoriteContext);
+    if (!favoriteIds) {
+        //お気に入りリストが未設定（取得前）であればお気に入りボタンは表示しない
+        return null;
+    }
+    const isFavorite = favoriteIds.indexOf(videoId) !== -1;
+
+    //サーバーへのリクエストが成功したか失敗したかに関わらず、レスポンスを待たずにページ上の
+    //お気に入り状態を変更する
+    const onClickHandler = (e) => {
+        e.stopPropagation();
+        //サーバーにお気に入りの追加または削除するリクエストを送る
+        api[isFavorite ? "delete" : "post"](videoId);
+        //サーバーからのレスポンスを待たずにお気に入り状態を変更する
+        dispatch({ type: isFavorite ? "remove" : "add ", id: videoId });
+    };
+    return presenter({
+        className,
+        isFavorite,
+        onClick: onClickHandler,
+    });
+};
+
+FavoriteButtonContainer.propTypes = {
+    className: PropTypes.string,
+    videoId: PropTypes.string.isRequired,
+    presenter: PropTypes.func.isRequired,
+    api: PropTypes.shape({
+        post: PropTypes.func,
+        delete: PropTypes.func,
+    }),
+};
+
+FavoriteButtonContainer.defaultProps = {
+    className: "",
+    api: {
+        post: (videoId) => axios.post(`/api/favorires/${videoId}`),
+        delete: (videoId) => axios.delete(`/api/favorites/${videoId}`),
+    },
+};
+
+export default (props) => (
+    <FavoriteButtonContainer
+        {...props}
+        presenter={FavoriteButtonPresenter}
+    />
+);
